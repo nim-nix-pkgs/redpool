a8773744456c04b58da4b8e78cb0a4d4338c0504
hugosenari <hugosenari@gmail.com>
2022-03-12 02:34:41 -0300

    chore: re index redpool

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Redis connection pool'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."redpool-master".url = "path:./master";
+  inputs."redpool-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."redpool-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Redis connection pool'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-redpool-master.flake = false;
+  inputs.src-redpool-master.owner = "zedeus";
+  inputs.src-redpool-master.ref   = "refs/heads/master";
+  inputs.src-redpool-master.repo  = "redpool";
+  inputs.src-redpool-master.type  = "github";
+  
+  inputs."https://github.com/zedeus/redis".dir   = "nimpkgs/h/https://github.com/zedeus/redis";
+  inputs."https://github.com/zedeus/redis".owner = "riinr";
+  inputs."https://github.com/zedeus/redis".ref   = "flake-pinning";
+  inputs."https://github.com/zedeus/redis".repo  = "flake-nimble";
+  inputs."https://github.com/zedeus/redis".type  = "github";
+  inputs."https://github.com/zedeus/redis".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."https://github.com/zedeus/redis".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-redpool-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-redpool-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
